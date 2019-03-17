cb1dd6f99ae3f26fc8a6c48b1c9598339d689ee7
Maria Wieckowska <mkw98@gazeta.pl>
2019-03-17 01:50:28 +0100

    fixes

--- client/modules/Lane/LaneContainer.js
+++ client/modules/Lane/LaneContainer.js
@@ -6,11 +6,11 @@ import { createLaneRequest, fetchLanes } from './LaneActions';
 import * as laneActions from './LaneActions';
 import { createNote } from '../Note/NoteActions';
 
-const mapStateToProps = (state, ownProps) => {
-  return {
-    laneNotes: ownProps.lane.notes.map(noteId => state.notes.find(note => note.id === noteId))
-  };
-};
+
+const mapStateToProps = (state, ownProps) => ({
+  laneNotes: ownProps.lane.notes.map(noteId => state.notes[noteId]).filter(noteId => noteId !== undefined),
+});
+
 
 const mapDispatchToProps = {
   ...laneActions,
